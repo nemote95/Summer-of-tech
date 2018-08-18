d9db7f2f9761b795e36cc5b91e522361121ce379
ConnorMattson <ConnorMattson@users.noreply.github.com>
2018-08-18 14:53:54 +1200

    Accounted for travelling on flat ground
    Previously just returned 0 energy expenditure when travelling on flat, now properly checks and sets the angle to 0.

--- src/Effort.js
+++ src/Effort.js
@@ -6,11 +6,11 @@
 // Returns:
 //   workDone in calories
 function getEnergyExpenditure(distance, changeInElevation) {
-	if (changeElevation <= 0) return 0;
+	if (changeElevation < 0) return 0;
 	let mass = 75 + 8; // Assuming average human weight of 75kg and bike weight of 8kg
 
 	// find the energy expended by elevation change
-	let angle = Math.atan(changeInElevation/distance);
+	let angle = (changeInElevation > 0) ? 0 : Math.atan(changeInElevation/distance);
 	let gravityForce = 9.81 * mass * Math.cos(angle);
 
 	// Find the energy expended by biking for that distance
