389df0711f3eb4bcf264ab0e74e116f6d422cc81
Negar <nemote95@gmail.com>
2018-08-19 11:50:46 +1200

    for loop to get lat and lngs of paths

--- src/directionsAPI.js
+++ src/directionsAPI.js
@@ -1,5 +1,6 @@
 function onMapsInit(){
-document.getElementById('go').onclick=calculateAndDisplayRoute;}
+	document.getElementById('go').onclick=calculateAndDisplayRoute;
+}
 
 
 function calculateAndDisplayRoute() {
@@ -11,10 +12,18 @@ function calculateAndDisplayRoute() {
           travelMode: 'BICYCLING'
         }, function(response, status) {
           if (status === 'OK') {
-			  var directionsArray=[];
-			  window.response =response.routes;
+			 var directionsArray=[];
+			  window.response =response;
 			  //value of this loop is 0 because its first value of response
-			  console.log(response);
+			  var route_number=0;
+		
+			for (var step of response.routes[0].legs[0].steps){
+				for(var path of step.path){
+				directionsArray.push({lat:path.lat(),lng:path.lng()});
+				
+				}
+              }
+			  console.log(directionsArray);
             directionsDisplay.setDirections(response);
           } else {
             window.alert('Directions request failed due to ' + status);
