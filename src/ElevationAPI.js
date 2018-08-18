e00ae3dd738d95fc5d609d0ae81f163ada6c0e66
Rslocs <randy.w.slocum@gmail.com>
2018-08-18 16:28:19 +1200

    Added in Array_of_Elevation function which returns the elvations into an array

--- src/ElevationAPI.js
+++ src/ElevationAPI.js
@@ -1,12 +1,19 @@
-function Elevation_URL(route){
+function Array_of_Elevation(results){
+    var elevation_arrays = [];
+    for(var elevaton of results){
+        elevation_arrays.push(results.elevation);
+    }
+    return elevation_arrays;
+}
+
+
+function Elevation_API(route){
     URL_Base = "https://maps.googleapis.com/maps/api/elevation/json?path=";
     api_Key = "&key=AIzaSyBHVHSBJ6sVHbvHCBIWcKMiXiYDqZoB-V8";
 
     for(var step of route.legs.steps){
         URL_Base += step.start_location + "|";
     }
-
     URL_Base += step.end_location + api_Key;
-    return URL_Base;
-
+    return Array_of_Elevation(URL_Base);  
 }
