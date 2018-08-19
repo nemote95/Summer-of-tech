853c17e4ab2b7c89855324356351554778730d6a
Oliver Jacks <olly.p.jacks@gmail.com>
2018-08-19 14:43:56 +1200

    refixed elevationAPI

--- src/ElevationAPI.js
+++ src/ElevationAPI.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-function Array_of_Elevation(results){
-    var elevation_arrays = [];
-    for(var elevaton of results){
-        elevation_arrays.push(results.elevation);
-    }
-    return elevation_arrays;
-}
-
-
-function Elevation_API(route){
-    URL_Base = "https://maps.googleapis.com/maps/api/elevation/json?path=";
-    api_Key = "&key=AIzaSyBHVHSBJ6sVHbvHCBIWcKMiXiYDqZoB-V8";
-
-    for(var step of route.legs.steps){
-        URL_Base += step.start_location + "|";
-    }
-    URL_Base += step.end_location + api_Key;
-    return Array_of_Elevation(URL_Base);
-=======
 function getDirectionalAPIData(path, distance, time) {
 	elevator.getElevationAlongPath({
           'path': path,
