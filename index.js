553acb2d0acd996add06f5393327d745579acb72
Negar <nemote95@gmail.com>
2020-03-12 22:43:14 +1300

    adding some logs to debug the server

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ app.get('/test', (req, res) => {
     res.render('index',{url: url})
 })
 
-app.listen(process.env.PORT || 5001)
-
+var server = app.listen(process.env.PORT || 5000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
