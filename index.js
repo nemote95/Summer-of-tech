9b0f524507d1dd8060ca95359c285c6ec3d27213
Negar <nemote95@gmail.com>
2020-03-13 14:05:43 +1300

    added limit to thenumber of calls based on timestamp

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-    start = new DateTime(2020, 03, 12, 13, 43, 00);
+    start = new Date(2020, 03, 12, 13, 43, 00);
     now = Date.now();
     if (now-start<362*24*60*60*1000){
         url = "https://maps.googleapis.com/maps/api/js?key="+process.env.API_KEY+"&libraries=places&callback=initMap";}
