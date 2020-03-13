ac50aa5a4f067a30c416d25eb94287bb63037096
Negar <nemote95@gmail.com>
2020-03-13 13:14:46 +1300

    fixed dependency

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var express = require('express');
-var app = express();
-
+const express = require('express')
 const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-
-app.get('/test', (req, res) => {
-
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => {
     url = "https://maps.googleapis.com/maps/api/js?key="+process.env.API_KEY+"&libraries=places&callback=initMap";
-
     res.render('index',{url: url})
-})
 
-var server = app.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
-  console.log("Express is working on port " + port);
-});
+})
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
