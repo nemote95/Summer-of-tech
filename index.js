bbec5b718ece272f149ad22f8159505cb4bb5e9e
Negar <nemote95@gmail.com>
2020-03-13 13:53:28 +1300

    added limit to thenumber of calls

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
-    url = "https://maps.googleapis.com/maps/api/js?key="+process.env.API_KEY+"&libraries=places&callback=initMap";
+    DateTime a = new DateTime(2020, 03, 12, 13, 43, 00);
+    DateTime b = Date.now();
+    if (b.Subtract(a).TotalMinutes<362*24*60){
+        url = "https://maps.googleapis.com/maps/api/js?key="+process.env.API_KEY+"&libraries=places&callback=initMap";}
+    else{ url=''}
     res.render('index',{url: url})
 
 })
